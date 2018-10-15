4b9877987fb95a5ce42c2eeaddf42a0445a41d51
Samuel Holland <samuel@sholland.org>
2018-10-14 21:30:48 -0500

    spl: Fix build with !CONFIG_SPL_SERIAL
    A large number of boards call preloader_console_init unconditionally.
    As the function is only defined with CONFIG_SPL_SERIAL, provide an
    empty fallback definition.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- include/spl.h
+++ include/spl.h
@@ -79,7 +79,11 @@ int spl_load_simple_fit(struct spl_image_info *spl_image,
 #define SPL_COPY_PAYLOAD_ONLY	1
 
 /* SPL common functions */
+#ifdef CONFIG_SPL_SERIAL_SUPPORT
 void preloader_console_init(void);
+#else
+static inline void preloader_console_init(void) {}
+#endif
 u32 spl_boot_device(void);
 u32 spl_boot_mode(const u32 boot_device);
 int spl_boot_partition(const u32 boot_device);
